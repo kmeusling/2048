a96f4651ef5b51cf3ebb1b090c2cc7037d8b0038
leonpalm <lpalm@virtu.com>
2015-03-17 12:31:25 +0000

    Constants.java: Match probability of 2 to the online game (10%).

--- src/games/Constants.java
+++ src/games/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
   public static final int GRID_SIZE = 4;
 
-  public static final double LIKELIHOOD_OF_2 = 0.2;
+  public static final double LIKELIHOOD_OF_2 = 0.1;
 
   public static final int BOT_DELAY_MS = 100;
 
