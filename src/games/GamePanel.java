98939ef84bce6cfbbc4bb3d279278efe8b2fd73b
leonpalm <lpalm@virtu.com>
2015-03-17 13:42:54 +0000

    GamePanel.java: Add a background grid

--- src/games/GamePanel.java
+++ src/games/GamePanel.java
@@ -35,17 +35,26 @@ public class GamePanel
 
     private static final int GRID_MIN_Y = ( WINDOW_HEIGHT - GRID_HEIGHT ) / 2;
 
-    private static final float GRID_SPACING_X = (float) GRID_WIDTH / GRID_SIZE;
+    // The actual distance between cells will be twice the padding.
+    private static final float CELL_PADDING_X = 6;
 
-    private static final float GRID_SPACING_Y = (float) GRID_WIDTH / GRID_SIZE;
+    private static final float CELL_PADDING_Y = 6;
 
-    private static final float CELL_CHAMFER_X = GRID_SPACING_X * 0.15f;
+    private static final float CELL_MIN_X = GRID_MIN_X + CELL_PADDING_X;
 
-    private static final float CELL_CHAMFER_Y = GRID_SPACING_Y * 0.15f;
+    private static final float CELL_MIN_Y = GRID_MIN_Y + CELL_PADDING_Y;
 
-    private static final float CELL_PADDING_X = GRID_SPACING_X * 0.05f;
+    private static final float CELL_CHAMFER_X = 20;
 
-    private static final float CELL_PADDING_Y = GRID_SPACING_Y * 0.05f;
+    private static final float CELL_CHAMFER_Y = 20;
+
+    private static final float GRID_SPACING_X = (GRID_WIDTH - 2 * CELL_PADDING_X) / GRID_SIZE;
+
+    private static final float GRID_SPACING_Y = (GRID_WIDTH - 2 * CELL_PADDING_Y) / GRID_SIZE;
+
+    private static final Color EMPTY_CELL_COLOR = new Color( 204, 192, 179 );
+
+    private static final Color GRID_COLOR = new Color( 187, 173, 160 );
 
     private final GameWindow gameWindow;
 
@@ -58,14 +67,16 @@ public class GamePanel
 
     private static void drawNumberInGrid( int logNumber, int gridX, int gridY, Graphics g ) {
 
-        g.setFont( new Font( "Helvetica Neue", Font.BOLD, getFontSize(logNumber) ) );
+        if( logNumber < 0 ) return;
+
+        g.setFont( new Font( "Helvetica Neue", Font.BOLD, getFontSize( logNumber ) ) );
 
         FontMetrics fm = g.getFontMetrics();
         String s = String.valueOf( 1 << logNumber );
 
-        int minX = (int) ( GRID_MIN_X + gridX * GRID_SPACING_X );
+        int minX = (int) ( CELL_MIN_X + gridX * GRID_SPACING_X );
         int x = (int) ( minX + ( GRID_SPACING_X - fm.stringWidth( s ) ) / 2 );
-        int minY = (int) ( GRID_MIN_Y + gridY * GRID_SPACING_Y );
+        int minY = (int) ( CELL_MIN_Y + gridY * GRID_SPACING_Y );
         int y = (int) ( minY + fm.getAscent() + ( GRID_SPACING_Y - fm.getHeight() ) / 2 );
         g.setColor( getColorForNumber( logNumber ) );
         g.drawString( s, x, y );
@@ -75,23 +86,28 @@ public class GamePanel
 
     private static int getFontSize( int logNumber ) {
 
-        if( logNumber < 7) return 48; // 1-64
-        if( logNumber < 11) return 32; // 128-512
-        if( logNumber < 14) return 24; // 4 digits
+        if( logNumber < 7 ) return 48; // 1-64
+        if( logNumber < 11 ) return 32; // 128-512
+        if( logNumber < 14 ) return 24; // 4 digits
         return 18; // 5 digits+
     }
 
 
-    private static Color getColorForNumber( int n ) {
+    private static Color getColorForNumber( int logNumber ) {
 
-        return n <= 5 ? BLACK : WHITE;
+        return logNumber <= 5 ? BLACK : WHITE;
     }
 
 
-    private static Color getColorForCell( int n ) {
+    private static Color getColorForCell( int logNumber ) {
 
-        if( n <= 11 ) {
-            int gComponent = 250 - ( 25 * n );
+        if( logNumber < 0 ) {
+
+            return EMPTY_CELL_COLOR;
+        }
+
+        if( logNumber <= 11 ) {
+            int gComponent = 250 - ( 25 * logNumber );
             return new Color( 255, gComponent, 0 );
         }
         else {
@@ -113,6 +129,30 @@ public class GamePanel
     }
 
 
+    private static void drawCells( int[] grid, Graphics g ) {
+
+        for( int i = 0; i < grid.length; i++ ) {
+            int logNumber = grid[ i ];
+            drawCell( logNumber, i % GRID_SIZE, i / GRID_SIZE, g );
+        }
+    }
+
+
+    private static void drawCell( int logNumber, int x, int y, Graphics g ) {
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setPaint( getColorForCell( logNumber ) );
+
+        float cellX = CELL_MIN_X + CELL_PADDING_X + x * GRID_SPACING_X;
+        float cellY = CELL_MIN_Y + CELL_PADDING_Y + y * GRID_SPACING_Y;
+        float cellWidth = GRID_SPACING_X - 2 * CELL_PADDING_X;
+        float cellHeight = GRID_SPACING_Y - 2 * CELL_PADDING_Y;
+        g2.fill( new RoundRectangle2D.Double( cellX, cellY, cellWidth, cellHeight, CELL_CHAMFER_X, CELL_CHAMFER_Y ) );
+
+        drawNumberInGrid( logNumber, x, y, g );
+    }
+
+
     @Override
     public Dimension getPreferredSize() {
 
@@ -125,33 +165,17 @@ public class GamePanel
 
         super.paintComponent( g );
         Game gameManager = gameWindow.gameManager;
+        drawBackground( g );
         drawCells( gameManager.getGameModel().getGrid(), g );
         drawWinLoseLabels( gameManager.getGameState(), g );
     }
 
 
-    private static void drawCells( int[] grid, Graphics g ) {
-
-        for( int i = 0; i < grid.length; i++ ) {
-            int logNumber = grid[ i ];
-            if( logNumber >= 0 ) {
-                drawCell( logNumber, i % GRID_SIZE, i / GRID_SIZE, g );
-            }
-        }
-    }
-
-
-    private static void drawCell( int logNumber, int x, int y, Graphics g ) {
+    private static void drawBackground( Graphics g ) {
 
         Graphics2D g2 = (Graphics2D) g;
-        g2.setPaint( getColorForCell( logNumber ) );
-
-        float cellX = GRID_MIN_X + CELL_PADDING_X + x * GRID_SPACING_X;
-        float cellY = GRID_MIN_Y + CELL_PADDING_Y + y * GRID_SPACING_Y;
-        float cellWidth = GRID_SPACING_X - 2 * CELL_PADDING_X;
-        float cellHeight = GRID_SPACING_Y - 2 * CELL_PADDING_Y;
-        g2.fill( new RoundRectangle2D.Double( cellX, cellY, cellWidth, cellHeight, CELL_CHAMFER_X, CELL_CHAMFER_Y ) );
-
-        drawNumberInGrid( logNumber, x, y, g );
+        g2.setPaint( GRID_COLOR );
+        g2.fill( new RoundRectangle2D.Double( (float) GRID_MIN_X, (float) GRID_MIN_Y, (float) GRID_WIDTH, (float) GRID_HEIGHT,
+                                              CELL_CHAMFER_X, CELL_CHAMFER_Y ) );
     }
 }
