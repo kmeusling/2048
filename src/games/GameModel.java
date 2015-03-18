feab29a896c327ce808c77352cc10d92d8e3713b
leonpalm <lpalm@virtu.com>
2015-03-18 17:50:18 +0000

    GameModel.java: Speed up number of free cells computation

--- src/games/GameModel.java
+++ src/games/GameModel.java
@@ -1,12 +1,8 @@
 package games;
 
-import com.google.common.collect.Lists;
-
 import java.util.Arrays;
-import java.util.List;
 
-import static games.Constants.LIKELIHOOD_OF_4;
-import static games.Constants.WINNING_POWER_OF_2;
+import static games.Constants.*;
 
 /**
  * Representation of the state of a game.
@@ -15,6 +11,8 @@ public class GameModel {
 
   // The width and height of the game grid
   private final int gridSize;
+
+  private static final NumberPlacer numberPlacer = new NumberPlacer();
 
   /*
      * A 1D representation of the log values of a corresponding 2D grid.
@@ -191,25 +189,6 @@ public class GameModel {
     return anyUpdates;
   }
 
-  /**
-   * Tries to add a new number to the grid according to the game rules.
-   * Returns true if a number was added, i.e. the board was not full.
-   */
-  public boolean addNumber() {
-    List<Integer> emptySpaces = Lists.newArrayList();
-    for (int i = 0; i < grid.length; i++) {
-      if (grid[i] == -1) {
-        emptySpaces.add(Integer.valueOf(i));
-      }
-    }
-
-    if (emptySpaces.isEmpty()) return false;
-
-    int index = (int) (Math.random() * emptySpaces.size());
-    byte numberToAdd = (byte) (Math.random() < LIKELIHOOD_OF_4 ? 2 : 1);
-    grid[emptySpaces.get(index).intValue()] = numberToAdd;
-    return true;
-  }
 
   /**
    * Returns true if the winning number has been reached.
@@ -241,5 +220,36 @@ public class GameModel {
       }
     }
     return true;
+  }
+
+  public void addNumber() {
+    numberPlacer.addNumber(grid);
+  }
+
+  private static class NumberPlacer {
+
+    private final int[] freeCells = new int[GRID_SIZE * GRID_SIZE];
+    private int numFreeCells;
+
+    /**
+     * Tries to add a new number to the grid according to the game rules.
+     * Returns true if a number was added, i.e. the board was not full.
+     */
+    public boolean addNumber(byte[] grid) {
+      numFreeCells = 0;
+      for (int i = 0; i < grid.length; i++) {
+        if (grid[i] == -1) {
+          freeCells[numFreeCells] = i;
+          numFreeCells++;
+        }
+      }
+
+      if (numFreeCells == 0) return false;
+
+      int index = (int) (Math.random() * numFreeCells);
+      byte numberToAdd = (byte) (Math.random() < LIKELIHOOD_OF_4 ? 2 : 1);
+      grid[freeCells[index]] = numberToAdd;
+      return true;
+    }
   }
 }
